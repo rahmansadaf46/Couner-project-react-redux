b1d122b8b7b3178bdf475b36e0e1acc8876baa5a
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2023-02-25 01:07:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import HooksCounter from "./components/HooksCounter";
+import DynamicHooksCounter from "./components/DynamicHooksCounter";
+import VariableCounter from "./components/VariableCounter";
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+                <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                    Simple Counter Application
+                </h1>
+
+                <div className="max-w-md mx-auto mt-10 space-y-5">
+                    {/* <HooksCounter id="1" /> */}
+                    {/* <Counter id="1" /> */}
+                    <HooksCounter />
+                    <DynamicHooksCounter />
+                    <VariableCounter />
+                    <VariableCounter dynamic />
+                </div>
+            </div>
+        </Provider>
+    );
+}
